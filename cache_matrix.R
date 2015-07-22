8e95bd71c1dee821493acca69d67a6f9c2ad57c6
Kelechi Nwachukwu <caesey2001@yahoo.com>
2015-07-22 14:16:05 -0400

    making sure the code for cachematrix.R and cache_matrix.R are the same

--- cache_matrix.R
+++ cache_matrix.R
@@ -1,8 +1,10 @@
-# This R function is supposed to cache potentially time-consuming computations.
-# concept is to look up in a cache, rather than recompute a non-changing
-# vector (or matrix) value.
+# This R function is supposed to cache potentially time-consuming computations,
+# such as matrix inversions and look up the value in the cache, when it is
+# called. It returns the cached value if the matrix has not be changed, else it
+# computes the new matrix inversion.
 
-# The function, make_cache_matrix, is to
+# makeCacheMatrix() creates a special "matrix", and certain callable functions
+# that enables caching (storing) and recall of information.
 
 makeCacheMatrix <- function(x = matrix())
 {
@@ -13,25 +15,38 @@ makeCacheMatrix <- function(x = matrix())
         m <<- NULL
     }
     get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
+
+    # Could also use ginv from MASS::ginv.
+    setInvMat <- function(solve) m <<- solve
+    getInvMat <- function() m
+
+    # Returns a list with the following formal argument and parameters
     list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
+         setInvMat = setInvMat,
+         getInvMat = getInvMat)
 }
 
-# Write a short comment describing this function
+# cacheSolve() calculates the inverse of a special "matrix" created with
+# the above function. However, it first checks to see if the inverse has
+# already been calculated. If so, it gets the inverse from the cache and
+# skips the computation. Otherwise, it calculates the inverse of the data
+# and sets the value of the inverse in the cache via the setInvMat function.
 
 cacheSolve <- function(x, ...)
 {
     # Return a matrix that is the inverse of 'x'
-    m <- x$getmean()
-    if(!is.null(m)) {
+    m <- x$getInvMat()
+    if(!is.null(m))
+    {
         message("getting cached data")
         return(m)
     }
     data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
-    m
+
+    # Calculating the new inverse of matrix
+    invMat <- solve(data, ...)
+    x$setInvMat(invMat)
+
+    # Returns the inverse of 'x'
+    invMat
 }
